41342c43e13fbec45ba39fe6f686eb92ceac7686
teemo <teemo@DESKTOP-JL25QLR>
2023-01-22 11:52:43 -0300

    new test deployment

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -7,24 +7,23 @@ export default function Contact() {
   return (
     <Container>
       <Content>
-        <a href="https://www.facebook.com/itaborahyink" target="_blank">
+     
         <ItemContact 
           IconFa={FaFacebook} 
           LinkContact="https://www.facebook.com/itaborahyink" 
         />
-        </a>
-        <a href="https://www.instagram.com/itaborahy_ink/" target="_blank">
+        
         <ItemContact 
           IconFa={FaInstagram} 
           LinkContact="https://www.instagram.com/itaborahy_ink/" 
         />
-        </a>
-        <a href="mailto:joeytafolla123@gmail.com" target="_blank">
+       
+       
         <ItemContact 
           IconFa={FaEnvelope} 
           LinkContact="itaboaryink@gmail.com" 
         />
-        </a>
+       
       </Content>
     </Container>
   )
